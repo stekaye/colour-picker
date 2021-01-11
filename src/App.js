3f3fce79188be15261545402cee4dfcdfe1003f2
Stephen Kaye <stephenkayebarcelona@gmail.com>
2021-01-11 16:51:20 +0100

    Add Chroma-js and ColorHelper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedPalettes from './seedPalettes';
+import {generatePalette} from './colorHelpers';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedPalettes[1]));
     return (
       <div className="App">
         <Palette {...seedPalettes[1]}/>
