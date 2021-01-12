46ac5bfe2341358d9c17268afe3042bf634bad78
Stephen Kaye <stephenkayebarcelona@gmail.com>
2021-01-12 12:35:41 +0100

    Add and style snackbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './NavBar.css';
 
@@ -9,14 +12,20 @@ class NavBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      format: "hex"
+      format: "hex",
+      open: false
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
 
-  handleChange(evt) {
-    this.setState({format: evt.target.value});
+  handleFormatChange(evt) {
+    this.setState({format: evt.target.value, open: true});
     this.props.handleChange(evt.target.value);
+  }
+
+  closeSnackbar() {
+    this.setState({open: false});
   }
 
   render() {
@@ -34,12 +43,25 @@ class NavBar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
+          <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar 
+          anchorOrigin={{vertical: "bottom", horizontal: "left"}} 
+          open={this.state.open}
+          autoHideDuration={2000}
+          message={<span id="message-id">Format Changed to {format.toUpperCase()}!</span>}
+          ContentProps={{"aria-describedby": "message-id"}}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton onClick={this.closeSnackbar} color="inherit" key="close" aria-label="close">
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </nav>
     )
   }
