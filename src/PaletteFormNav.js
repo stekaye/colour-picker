3a52f177983608dc380d3b1b11cc94c9d2f42f0e
Stephen Kaye <stephenkayebarcelona@gmail.com>
2021-01-18 11:29:04 +0100

    Fix form dialog and install Emoji Picker

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -54,10 +54,15 @@ class PaletteFormNav extends Component {
     super(props);
     this.state = {formShowing: false}
     this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
   }
 
-showForm() {
+  showForm() {
     this.setState({ formShowing: true })
+  }
+
+  hideForm() {
+    this.setState({ formShowing: false })
   }
 
   render() {
@@ -96,7 +101,7 @@ showForm() {
             </div>
         </AppBar>
 
-        {this.state.formShowing && <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit}/>}
+        {this.state.formShowing && <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} hideForm={this.hideForm}/>}
 
       </div>
     )
